5f91a27e91fb899c1faa71154ef312de7f487fa2
Iyxan23 <nurihsanalghifari@gmail.com>
2021-12-11 11:43:04 +0700

    Initialize the app

---
+++ android-app/app/src/main/java/com/iyxan23/slice/ListActivity.kt
@@ -0,0 +1,11 @@
+package com.iyxan23.slice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list)
+    }
+}
