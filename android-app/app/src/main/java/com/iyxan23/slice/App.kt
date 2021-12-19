55b72f1cd6b0590edb121b71b3ce81c2a7a0fb3f
Iyxan23 <nurihsanalghifari@gmail.com>
2021-12-19 14:03:32 +0700

    app: make the url change to debug url on debug builds

--- android-app/app/src/main/java/com/iyxan23/slice/App.kt
+++ android-app/app/src/main/java/com/iyxan23/slice/App.kt
@@ -1,6 +1,7 @@
 package com.iyxan23.slice
 
 import android.app.Application
+import com.iyxan23.slice.shared.DEBUG_URL
 import com.iyxan23.slice.shared.SERVER_URL
 import io.socket.client.IO
 import io.socket.client.Socket
@@ -9,5 +10,5 @@ class App : Application() {
     /**
      * The socket we use to communicate with the server
      */
-    val socket: Socket = IO.socket(SERVER_URL)
+    val socket: Socket = IO.socket(if (BuildConfig.DEBUG) DEBUG_URL else SERVER_URL)
 }
--- android-app/app/src/main/java/com/iyxan23/slice/shared/constants.kt
+++ android-app/app/src/main/java/com/iyxan23/slice/shared/constants.kt
@@ -1,3 +1,10 @@
 package com.iyxan23.slice.shared
 
 const val SERVER_URL = "https://skpro-contest-slice-backend.herokuapp.com"
+
+// a local development server running on the host on 127.0.0.1:8080
+// read more: https://developer.android.com/studio/run/emulator-networking#networkaddresses
+const val DEBUG_URL = "https://10.0.2.2:8080"
+
+const val SOCKET_EVENT_CONTROLLER_CONNECT = "controller connect"
+const val SOCKET_EMIT_CREATE_SESSION = "create session"
