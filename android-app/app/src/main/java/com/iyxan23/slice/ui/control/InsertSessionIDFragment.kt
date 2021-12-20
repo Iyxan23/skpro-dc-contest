40084cde4daa8f9de99af7839495c7e05d6df6ec
Iyxan23 <nurihsanalghifari@gmail.com>
2021-12-20 12:06:53 +0700

    app: create InsertSessionIDFragment & RemoteControlFragment

---
+++ android-app/app/src/main/java/com/iyxan23/slice/ui/control/InsertSessionIDFragment.kt
@@ -0,0 +1,10 @@
+package com.iyxan23.slice.ui.control
+
+import androidx.fragment.app.Fragment
+import com.iyxan23.slice.R
+import com.iyxan23.slice.databinding.FragmentInsertSessionIdBinding
+import com.zhuinden.fragmentviewbindingdelegatekt.viewBinding
+
+class InsertSessionIDFragment : Fragment(R.layout.fragment_insert_session_id) {
+    private val binding by viewBinding(FragmentInsertSessionIdBinding::bind)
+}
---
+++ android-app/app/src/main/java/com/iyxan23/slice/ui/control/RemoteControlFragment.kt
@@ -0,0 +1,10 @@
+package com.iyxan23.slice.ui.control
+
+import androidx.fragment.app.Fragment
+import com.iyxan23.slice.R
+import com.iyxan23.slice.databinding.FragmentRemoteControlBinding
+import com.zhuinden.fragmentviewbindingdelegatekt.viewBinding
+
+class RemoteControlFragment : Fragment(R.layout.fragment_remote_control) {
+    private val binding by viewBinding(FragmentRemoteControlBinding::bind)
+}
