acb51231efb746d45c80b0c3ce0bf2c44dd636c8
Iyxan23 <nurihsanalghifari@gmail.com>
2021-12-21 15:20:46 +0700

    app: rename CreateSessionResponse children's serial name to lowercase

--- android-app/app/src/main/java/com/iyxan23/slice/domain/models/response/CreateSessionResponse.kt
+++ android-app/app/src/main/java/com/iyxan23/slice/domain/models/response/CreateSessionResponse.kt
@@ -1,12 +1,15 @@
 package com.iyxan23.slice.domain.models.response
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 sealed class CreateSessionResponse {
     @Serializable
+    @SerialName("success")
     data class Success(val token: String, val sessionId: String) : CreateSessionResponse()
 
     @Serializable
+    @SerialName("error")
     data class Error(val message: String) : CreateSessionResponse()
 }
