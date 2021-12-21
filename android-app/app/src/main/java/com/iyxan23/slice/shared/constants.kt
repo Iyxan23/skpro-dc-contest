89629e07e43e3b562b918ef771790a987ca4bfea
Iyxan23 <nurihsanalghifari@gmail.com>
2021-12-21 14:07:48 +0700

    app: add comments to SOCKET_ constants

--- android-app/app/src/main/java/com/iyxan23/slice/shared/constants.kt
+++ android-app/app/src/main/java/com/iyxan23/slice/shared/constants.kt
@@ -8,9 +8,20 @@ const val SERVER_URL = "wss://skpro-contest-slice-backend.herokuapp.com"
 // read more: https://developer.android.com/studio/run/emulator-networking#networkaddresses
 const val DEBUG_URL = "wss://10.0.2.2:8080"
 
+// socket.io events ==
+
+// an event for the remote when a controller is connecting to the session ()
 const val SOCKET_CONTROLLER_CONNECT = "controller connect"
+
+// an emit event for the remote when it wants to create a session
 const val SOCKET_CREATE_SESSION = "create session"
+
+// an emit event for the controller when it wants to connect to a session created by a remote
 const val SOCKET_CONNECT_SESSION = "connect session"
+
+// an event and emit event for both remote and controller
+// event: dispatched when the other side set their ICE
+// emit: when we want to set our ice
 const val SOCKET_SET_ICE = "set ice"
 
 val iceServers = listOf(PeerConnection.IceServer("stun:stun3.l.google.com:19302"))
