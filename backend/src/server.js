1fcb0699f524b2a13e9a6e43ba0a90d709e7c9c2
Iyxan23 <nurihsanalghifari@gmail.com>
2021-12-22 14:05:50 +0700

    backend: implement the new flowchart
    confirmation & using room as an identifier if a socket connected to a room

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,6 +12,9 @@ const supabase =
 
 // Socket.IO fun stuff =================
 
+// things you need to know:
+// rooms[1] is used to store what session the socket is connected to
+
 io.on('connection', socket => {
   console.log(`${socket.id} has just connected`);
 
@@ -23,21 +26,25 @@ io.on('connection', socket => {
 
     const { error } = await supabase
       .from('sessions')
-      .insert([{'id': session_id, 'remote_socket_id': socket.id}]);
+      .insert({'id': session_id, 'remote_socket_id': socket.id});
 
     if (error) {
       console.error(`err while inserting a new session row: ${error}`);
 
       ack({'type': 'error', 'message': error.toString()});
+
     } else {
-      ack({'type': 'success', 'token': remote_token, 'session_id': session_id});
+      // put this socket to a room of its session id
+      socket.join(session_id);
+
+      ack({'type': 'success', 'session_id': session_id});
     }
   });
 
   // called when a controller wanted to connect to a session
   socket.on('connect session', async (session_id, ack) => {
     // check if the provided session id exists
-    const { data, error } = await supabase
+    const { data, error: err1 } = await supabase
       .from('sessions')
       .select('*')
       .eq('id', session_id)
@@ -47,26 +54,129 @@ io.on('connection', socket => {
       ack({'type': 'error', 'message': 'Invalid session ID'})
       return
 
-    } else if (error) {
-      console.error(`err while searching for session id ${session_id}: ${error}`)
+    } else if (err1) {
+      console.error(`err while searching for session id ${session_id}: ${err1}`)
 
+      ack({'type': 'error', 'message': err1.toString()})
+      return
+    }
+
+    // it exists, we update the row to include this socket's id & set the status to be confirming
+    const { error: err2 } = await supabase
+      .from('sessions')
+      .update({'controller_socket_id': socket.id, 'status': 'CONFIRMING'})
+      .eq('id', session_id)
+
+    if (err2) {
+      console.error(`err while updating controller_socket_id of ${session_id}: ${err2}`)
+      ack({'type': 'error', 'message': err2.toString})
+      return
+    }
+
+    // success!
+    ack({'type': 'success'})
+
+    // now we ask the remote if they confirm the remote control connection
+    socket.to(data.remote_socket_id).emit('controller connect confirm')
+  });
+
+  // called when the remote confirms connection from the controller
+  socket.on('confirm connection', async (ack) => {
+    // since this can only be called by a remote, we're going to find the session
+    // this remote is in
+    const { data, error: err1 } = await supabase
+      .from('sessions')
+      .select('*')
+      .eq('remote_socket_id', socket.id)
+
+    if (!data) {
+      ack({'type': 'error', 'message': 'Has not joined any session'})
+      return
+
+    } else if (err1) {
+      console.error(`err on confirm connection for ${socket.id}: ${err1}`)
+      ack({'type': 'error', 'message': err1.toString()})
+      return
+    }
+
+    // session found! update the session status to CONFIRMED
+    const { error: err2 } = await supabase
+      .from('sessions')
+      .update({'status': 'CONFIRMED'})
+      .eq('id', data.id)
+
+    if (err2) {
+      console.error(`err on confirming for ${socket.id}: ${err2}`)
+      ack({'type': 'error', 'message': err2.toString()})
+      return
+    }
+
+    // success!
+    ack({'type': 'success'})
+
+    // and put the controller socket to the room according to its session id
+    io.sockets.sockets.get(data.controller_socket_id).join(session_id)
+
+    // then tell the controller socket that the connection has been confirmed
+    socket.to(data.controller_socket_id).emit('connection confirmed')
+  })
+
+  // when this socket sets its ice
+  socket.on('set ice', async (ice, ack) => {
+
+    // check if they have connected to any session yet
+    if (!socket.rooms[1]) {
+      // they haven't connected to any session yet!
+      ack({'type': 'error', 'message': 'Has not connected to any session'})
+      return
+    }
+
+    const session_id = socket.rooms[1]
+
+    // get the session data
+    const { data, error } = await supabase
+      .from('sessions')
+      .select('*')
+      .eq('id', session_id)
+    
+    if (error) {
+      console.error(`failed to set ice for socket ${socket.id} on session id ${session_id}: ${error}`)
       ack({'type': 'error', 'message': error.toString()})
       return
     }
 
-    // it exists, we update the row to include this socket's id
-    const { error } = await supabase
-      .from('sessions')
-      .update({'controller_socket_id': socket.id})
-      .match({'id': session_id})
+    // check what side are we, then send the ice to the opposite side
+    if (socket.id === data.remote_socket_id) {
+      // this is the remote trying to send the ICE to controller
+      // but first check if the other side has connected to the session yet
+      if (!data.controller_socket_id) {
+        // nope, they haven't, return an error
+        ack({'type': 'error', 'message': 'Other peer hasn\'t connected to this session yet'})
+        return
+      }
 
-    if (error) {
-      console.error(`err while updating controller_socket_id of ${session_id}: ${error}`)
-      ack({'type': 'error', 'message': error.toString})
+      // cool, they're there, send them the ice!
+      io.sockets.sockets.get(data.controller_socket_id).emit('set ice', ice)
+
+    } else if (socket.id === data.controller_socket_id) {
+      // this is the controller trying to send the ICE to the remote
+      // then send it to them!
+      io.sockets.sockets.get(data.remote_socket_id).emit('set ice', ice)
+
     } else {
-      ack({'type': 'success'})
+      // ?? what socket is this, its neither the controller nor the remote but its
+      // connected to the session!?!??
+      // remove them from the room session id & return
+      console.error(`who is this socket ${socket.id}, data: ${JSON.stringify(data)}`)
+
+      socket.leave(session_id)
+      ack({'type': 'error', 'message': 'WHO THE HECK ARE YOU'})
+      return
     }
-  });
+
+    // success!
+    ack({'type': 'success'})
+  })
 });
 
 io.on('disconnect', socket => {
