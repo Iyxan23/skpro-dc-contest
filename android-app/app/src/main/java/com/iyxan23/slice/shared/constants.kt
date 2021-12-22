ad8ffcd74e453a4d3bd35f5abf8f8afe988f535e
Iyxan23 <nurihsanalghifari@gmail.com>
2021-12-22 14:52:10 +0700

    app: add a lot of doc on socket.io events in constants.kt

--- android-app/app/src/main/java/com/iyxan23/slice/shared/constants.kt
+++ android-app/app/src/main/java/com/iyxan23/slice/shared/constants.kt
@@ -9,14 +9,37 @@ const val SERVER_URL = "https://skpro-contest-slice-backend.herokuapp.com"
 const val DEBUG_URL = "http://10.0.2.2:8080"
 
 // socket.io events ==
+// if you're confused, take a look at the diagram in github
 
-// an event for the remote when a controller is connecting to the session ()
-const val SOCKET_CONTROLLER_CONNECT = "controller connect"
-
+// REMOTE - create session -> SERVER
+//
 // an emit event for the remote when it wants to create a session
 const val SOCKET_CREATE_SESSION = "create session"
 
-// an emit event for the controller when it wants to connect to a session created by a remote
+// SERVER - controller connect confirm -> REMOTE
+//
+// an event for the remote when a controller is trying to connect to the session and the server is
+// asking for a conformation for the remote if it wants to get connected to this specific controller
+//
+// the remote will then respond back by emitting SOCKET_CONFIRM_CONNECTION
+const val SOCKET_CONTROLLER_CONNECT_CONFIRM = "controller connect confirm"
+
+// REMOTE - confirm connection -> SERVER
+//
+// an event that gets emitted by the remote when it confirms a connection from a specific controller
+// this is called after SOCKET_CONTROLLER_CONNECT_CONFIRM
+const val SOCKET_CONFIRM_CONNECTION = "confirm connection"
+
+// SERVER - connection confirmed -> CONTROLLER
+//
+// an event for the controller when its connection is confirmed by the remote, this is called by the
+// server when the REMOTE emits SOCKET_CONFIRM_CONNECTION
+const val SOCKET_CONNECTION_CONFIRMED = "connection confirmed"
+
+// CONTROLLER - connect session -> SERVER
+//
+// an event that gets emitted by the controller when it wants to connect to a session created by a
+// remote
 const val SOCKET_CONNECT_SESSION = "connect session"
 
 // an event and emit event for both remote and controller
