2ca08c4e708029c14f6daadcab564a3f9981bdf9
Iyxan23 <nurihsanalghifari@gmail.com>
2021-12-22 14:05:07 +0700

    backend: fix session id generating non-numbers

--- backend/src/server.js
+++ backend/src/server.js
@@ -75,7 +75,7 @@ io.on('disconnect', socket => {
 
 function generate_id(length) {
     let result = '';
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const characters = '1234567890';
     for (let i = 0; i < length; i++) {
       result += characters.charAt(Math.floor(Math.random() * characters.length));
     }
